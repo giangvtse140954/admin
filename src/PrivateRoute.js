4660b7bb20ffdd31ea23dee0c9cda4218cf72250
Vu Thu Giang <vuthugiang2610@gmail.com>
2021-07-02 21:55:01 +0700

    hh

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const isLogin = localStorage.getItem('isLogin');
   return (
     <Route
       {...rest}
       render={(props) => {
-        return isLogin ? (
+        return true ? (
           <div>
             <Component {...props} />
           </div>
