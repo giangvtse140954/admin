a9a3be34fc556c2bc1e2dee1316edee4cd00c310
Vu Thu Giang <vuthugiang2610@gmail.com>
2021-07-12 08:03:59 +0700

    fa

--- src/GarageList.js
+++ src/GarageList.js
@@ -143,7 +143,6 @@ const GarageList = () => {
   const [selected, setSelected] = useState([]);
   const [open, setOpen] = React.useState(false);
   const [isOpen, setIsOpen] = React.useState(false);
-  const [status, setStatus] = React.useState(true);
 
   let history = useHistory();
   const handleOpen = () => {
